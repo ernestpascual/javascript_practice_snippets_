ea4185f2a90340b380df9d0f744b0abb22bb2d20
ernestpascual <eepascual@gmail.com>
2017-03-04 11:03:37 +0800

    Update projectEulerProblem4.js

--- projectEulerProblem4.js
+++ projectEulerProblem4.js
@@ -1,10 +1,15 @@
+// intialize variables
 var x = 0;
 var y = "";
 var answer  = 0;
+
+// start dual for loops for multiplying the product
 for(i=100; i < 1000; i++){
   for (j=100; j< 1000; j++){
      x = (i*j)
+    // reversing a string
      y = x.toString().split("").reverse().join("");
+    // remember to parse y so you can compare inside if statement
     	if (x===parseInt(y)){
           if (x > parseInt(answer)){
       answer = x;
@@ -14,3 +19,31 @@ for(i=100; i < 1000; i++){
 
 }
   console.log(answer);
+
+
+// reusable number palindrome function on the highest x digit number
+
+var highestNumberPalindrome = function (number){
+  // get the lowest and highest number for the range
+  var highestNumber = Math.pow(10, number);
+  var lowestNumber  = Math.pow(10, number -1)
+  
+  // apply the operation
+  for(i=lowestNumber; i < highestNumber; i++){
+  for (j=lowestNumber; j< highestNumber; j++){
+     x = (i*j)
+    // reversing a string
+     y = x.toString().split("").reverse().join("");
+    // remember to parse y so you can compare inside if statement
+    	if (x===parseInt(y)){
+          if (x > parseInt(answer)){
+      answer = x;
+    }
+        }
+        }
+ return answer;
+}
+}
+  
+console.log(highestNumberPalindrome(2));
+ 
