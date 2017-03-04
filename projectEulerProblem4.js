8381cd31c21b2eaa804b190547a6388cd7b0223e
ernestpascual <eepascual@gmail.com>
2017-03-04 10:48:29 +0800

    Update projectEulerProblem4.js

--- projectEulerProblem4.js
+++ projectEulerProblem4.js
@@ -1,15 +1,13 @@
 var x = 0;
 var y = "";
-var z = "";
 var answer  = 0;
 for(i=100; i < 1000; i++){
   for (j=100; j< 1000; j++){
      x = (i*j)
      y = x.toString().split("").reverse().join("");
-     z = x.toString();
-    	if (z===y){
-          if (parseInt(z) > parseInt(answer)){
-      answer = z;
+    	if (x===parseInt(y)){
+          if (x > parseInt(answer)){
+      answer = x;
     }
         }
         }
